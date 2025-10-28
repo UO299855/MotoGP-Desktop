6ae280832cdb2c6fc2e842dcfa46accaa7408102
Javier <javier.ortin.rodenas@gmail.com>
2025-10-28 10:16:33 +0100

    FlipCard funcionando

--- MotoGP-Desktop/js/memoria.js
+++ MotoGP-Desktop/js/memoria.js
@@ -1,9 +1,20 @@
 class Memoria {
     constructor() {
-
+        /**
+         * Registra el listener de evento de click pertinente para las cartas
+         */
+        const cartas = document.querySelectorAll("article");
+        for(let i = 0; i < cartas.length; i++) {
+            cartas[i].onclick = this.flipCard
+        }
     }
 
+    /**
+     * Le da la vuelta a la carta que se pasa como parámetro
+     * @param {Element} card Carta parámetro
+     */
     flipCard(card) {
-        card.
+        console.log(this)
+        card.setAttribute("data-state", "flip")
     }
 }
