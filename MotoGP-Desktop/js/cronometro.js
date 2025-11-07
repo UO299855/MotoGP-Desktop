0424d8fada490b0d342f81a04d6cf3f06fc7f154
Javier <javier.ortin.rodenas@gmail.com>
2025-11-07 14:12:45 +0100

    Ejercicio 2 tarea

--- MotoGP-Desktop/js/cronometro.js
+++ MotoGP-Desktop/js/cronometro.js
@@ -1,6 +1,7 @@
 class Cronometro {
     #inicio
     #tiempo
+    #corriendo
 
     constructor() {
         this.#tiempo = 0
@@ -13,7 +14,9 @@ class Cronometro {
             this.#inicio = Date.now()
         }
         //Llamada a 'actualizar()' con el contexto del cronómetro cada décima de segundo
-        setInterval(this.actualizar.bind(this), 100)
+        if(this.#corriendo == null) {   
+            this.#corriendo = setInterval(this.actualizar.bind(this), 100)
+        }
     }
 
     actualizar() {
@@ -25,6 +28,19 @@ class Cronometro {
         this.mostrar()
     }
 
+    reiniciar() {
+        this.parar()
+        this.#tiempo = 0
+        this.mostrar()
+    }
+
+    parar() {
+        if(this.#corriendo != null) {
+            clearInterval(this.#corriendo)
+            this.#corriendo = null
+        }
+    }
+
     _formatTwoPlaceNumber(quantity) {
         return `${quantity < 10 ? "0" : ""}${quantity}`;
     }
@@ -33,10 +49,10 @@ class Cronometro {
      * Escribe los datos del cronómetro en el primer párrafo hijo del elemento <main>
      */
     mostrar() {
-        const parrafo = document.querySelector("main > p"); //querySelector ya toma el primero, no hay que añadir ":first-of-type"
+        const parrafo = document.querySelector("main p"); //querySelector ya toma el primero, no hay que añadir ":first-of-type"
         const minutos = Math.floor(this.#tiempo / 60000); //truncamos
         const segundos = Math.floor(this.#tiempo/1000 - 60 * minutos);
-        const decimas = Math.floor((this.#tiempo - 60000 * minutos - 1000*segundos));
-        parrafo.textContent = `${minutos}:${segundos}.${decimas}`
+        const decimas = Math.floor((this.#tiempo - 60000 * minutos - 1000*segundos)/100);
+        parrafo.textContent = `${this._formatTwoPlaceNumber(minutos)}:${this._formatTwoPlaceNumber(segundos)}.${decimas}`
     }
 }
--- MotoGP-Desktop/js/memoria.js
+++ MotoGP-Desktop/js/memoria.js
@@ -1,5 +1,11 @@
 class Memoria {
+    #tablero_bloqueado
+    #primera_carta
+    #segunda_carta
+    
     constructor() {
+        this.reiniciarAtributos()
+        
         /**
          * Registra el listener de evento de click pertinente para las cartas
          */
@@ -15,6 +21,33 @@ class Memoria {
      */
     flipCard(card) {
         console.log(card) //TODO borrar el logeo
-        card.setAttribute("data-state", "flip")
+        card.setAttribute("data-estado", "volteada")
+    }
+
+    reiniciarAtributos() {
+        this.#tablero_bloqueado = true
+        this.#primera_carta = null
+        this.#segunda_carta = null
+    }
+
+    barajarCartas() {
+        let cartas = Array.from(document.querySelectorAll("article"))
+        const main = document.querySelector("main")
+        for(let i = 0; i < cartas.length; i++) {
+            let rIndex = Math.floor(Math.random() * (cartas.length - i))
+            let eliminada = cartas.splice(rIndex, 1)[0];
+            main.appendChild(eliminada)
+        }
+    }
+
+    deshabilitarCartas() {
+        this.#primera_carta.setAttribute("data-estado", "revelada")
+        this.#segunda_carta.setAttribute("data-estado", "revelada")
+        this.comprobarJuego()
+        this.reiniciarAtributos()
+    }
+
+    comprobarJuego() {
+        return 12 == document.querySelectorAll("main article[data-estado=revelada]").length
     }
 }
