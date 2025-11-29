24a9c4cb762348ad1f3070f754c70a7b076a0bc7
Javier <javier.ortin.rodenas@gmail.com>
2025-11-29 13:44:06 +0100

    Cambios css plegas nav

--- js/plegarNav.js
+++ js/plegarNav.js
@@ -1,26 +1,30 @@
 class Plegar {
     #button
     #nav
-    #visible
+    #expanded
     
     constructor() {
         this.#nav = document.querySelector("header > nav")
         this.#button = document.querySelector("header button")
         this.#button.addEventListener("click", this.#toggle.bind(this))
-
         // Garantizamos la adaptabilidad si se redimensiona la ventana
         let matchMedia = window.matchMedia("(min-width: 320px) and (max-width: 799px)")
         matchMedia.addEventListener("change", this.#resize.bind(this))
+
+        this.#expanded = matchMedia.matches
+        this.#toggle()
     }
 
     /** Muestra u oculta el menú de navegación (nav del header) al pulsar el botón*/
     #toggle() {
-        this.#nav.style.display =  this.#nav.checkVisibility()?"none":"flex"
+        this.#expanded = !this.#expanded
+        this.#nav.setAttribute("aria-expanded", this.#expanded)
     }
 
     /** Muestra u oculta el menú de navegación al redimensionar la ventana */
     #resize(e) {
-        // e.matches devuelve true si el documento cumple la mediaquery inicial
-        this.#nav.style.display =  e.matches ? "none" : "flex"
+        // e.matches devuelve true si el documento cumple la mediaquery
+        this.#expanded =  e.matches
+        this.#toggle()
     }
 }
