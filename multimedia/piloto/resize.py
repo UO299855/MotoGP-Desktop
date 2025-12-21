9800317509ebe82de7377b46825c87ad7e0f124b
Javier <javier.ortin.rodenas@gmail.com>
2025-12-21 21:13:09 +0100

    Cambio foto MIR

--- multimedia/piloto/resize.py
+++ multimedia/piloto/resize.py
@@ -5,9 +5,9 @@ image_path = "joan-mir.jpg"  # Cambia esto por la ruta de tu imagen
 img = Image.open(image_path)
 
 # Redimensiona la imagen (por ejemplo, a 800x600 píxeles)
-new_width = 675
-new_height = 448
+new_width = 1020
+new_height = 680
 resized_img = img.resize((new_width, new_height))
 
 # Guarda la nueva imagen redimensionada
-resized_img.save("joan-mir-tablet.jpg")  # El nombre del archivo nuevo
+resized_img.save("joan-mir.jpg")  # El nombre del archivo nuevo
