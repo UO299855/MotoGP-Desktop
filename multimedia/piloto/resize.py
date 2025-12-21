719f2dae4538cb7d1a8ab5fa67fa8a2a95d1f4fe
Javier <javier.ortin.rodenas@gmail.com>
2025-12-21 21:08:35 +0100

    Cambios dimension imagenes

---
+++ multimedia/piloto/resize.py
@@ -0,0 +1,13 @@
+from PIL import Image
+
+# Abre la imagen
+image_path = "joan-mir.jpg"  # Cambia esto por la ruta de tu imagen
+img = Image.open(image_path)
+
+# Redimensiona la imagen (por ejemplo, a 800x600 píxeles)
+new_width = 675
+new_height = 448
+resized_img = img.resize((new_width, new_height))
+
+# Guarda la nueva imagen redimensionada
+resized_img.save("joan-mir-tablet.jpg")  # El nombre del archivo nuevo
